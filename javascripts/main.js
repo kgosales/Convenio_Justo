c1b7a7d6b62ed481e4464ff51622f95ce6c5e10b
Gabriel Sales <kleythongabriel@outlook.com>
2024-12-21 10:44:08 -0300

    0.0.0.5

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,3 +7,35 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const addOptionsUF = () => {
+    const selectUF = document.querySelector("#uf")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then(response => response.json())
+        .then(data => {
+            data.sort((a, b) => a.sigla.localeCompare(b.sigla))
+            data.forEach(uf => {
+                const option = document.createElement("option")
+                option.value = uf.sigla
+                option.text = uf.sigla
+                selectUF.appendChild(option)
+            })
+        })
+}
+
+const addOptionOperadoras = () => {
+    const selectOperadoras = document.querySelector("#operadoras")
+    const operadoras = ["Amil", "Unimed", "Bradesco", "Sulamerica"]
+
+    operadoras.sort((a, b) => a.localeCompare(b))
+    operadoras.forEach(operadora => {
+        const option = document.createElement("option")
+        option.value = operadora
+        option.text = operadora
+        selectOperadoras.appendChild(option)
+    })
+}
+
+addOptionsUF()
+addOptionOperadoras()
